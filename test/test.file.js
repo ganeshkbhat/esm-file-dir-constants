7918fe832de708d43980dd6ae327af8845839a91
Ganesh B <ganeshsurfs@gmail.com>
2024-02-12 19:00:13 +0530

    [COMMIT] Adding __filename and __dirname constants for esm packages

--- test/test.file.js
+++ test/test.file.js
@@ -19,10 +19,35 @@
 
 import test from "node:test";
 import { strict as assert } from "node:assert";
-import {test as lib} from "./index.js";
+import { test as lib } from "./index.js";
 
-test('synchronous passing test', (t) => {
-	const result = lib("World");
-  assert.strictEqual(result, "Hello World");
+import { default as __filename } from "../src/polyfill.__filename.js";
+import { default as __dirname } from "../src/polyfill.__dirname.js";
+import { getDirname, getFilename } from "../src/polyfill.__dirname.regex.js";
+
+
+// test('synchronous passing test', (t) => {
+// 	const result = lib("World");
+//   assert.strictEqual(result, "Hello World");
+// });
+
+test('check if __filename is defined', (t) => {
+  const result = __filename;
+  assert.strictEqual(result, __filename);
+});
+
+test('check if __dirname is defined', (t) => {
+  const result = __dirname;
+  assert.strictEqual(result, __dirname);
+});
+
+test('check if getDirname is defined', (t) => {
+  const result = getDirname();
+  assert.strictEqual(result, __dirname);
+});
+
+test('check if getFilename is defined', (t) => {
+  const result = getFilename();
+  assert.strictEqual(result, __filename);
 });
 
